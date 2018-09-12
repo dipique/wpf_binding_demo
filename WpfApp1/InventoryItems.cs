c6d93233db7c48415618ecca37740c53bd1be6c8
Daniel Kaschel <dipique@gmail.com>
2018-09-12 09:03:02 -0500

    Initial Upload

---
+++ WpfApp1/InventoryItems.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using static System.Reflection.BindingFlags;
+
+namespace WpfApp1
+{
+    public class InventoryItem
+    {
+        public string Serial_Number { get; set; } = "SN";
+        public string Make { get; set; } = "mk";
+        public string Model { get; set; } = "model";
+        public string Deployed_To { get; set; } = "dt";
+        public string Machine_Name { get; set; } = "mn";
+        public string Conway_Tag { get; set; } = "ct";
+        public string Status { get; set; } = "status";
+        public string Notes { get; set; } = "notes";
+
+        public InventoryItem() { }
+        public InventoryItem(object anonymousObject)
+        {
+            //get the type of the argument
+            var anonType = anonymousObject.GetType();
+
+            //make sure it's an anonymous type
+            if (anonType.Namespace != null) return;
+
+            //loop through every public property and try to match it to the anonymous type
+            foreach(var prop in GetType().GetProperties(Public | Instance))
+            {
+                //see if the anonymous type has a matching property (null does not pattern match)
+                if (anonType.GetProperty(prop.Name) is PropertyInfo anonProp)
+                {
+                    //if we found matching properties, set the value! This will fail if there are
+                    //properties with matching names that have different types.
+                    prop.SetValue(this, anonProp.GetValue(anonymousObject));
+                }
+            }
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<InventoryItem> db = Enumerable.Range(1, 100).Select(i => new InventoryItem()).ToList();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //ignore the details of the query, it's just so I have a data source
+            var query = (from a in db
+                         where a.Conway_Tag != null
+                         select a).ToList();
+
+            //set the form data context
+            DataContext = query;
+        }
+
+        private void dgInventory_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (dgInventory.SelectedItem is InventoryItem i)
+            {
+                var sw = new Modify_Form();
+                sw.DataContext = i;
+                sw.Show();
+            }
+            //if (sender is DataGridRow dg) //make sure we really are dealing with a DataGridRow
+            //    if (dg.IsSelected) //make sure the item is selected
+            //        if (dg.Item is InventoryItem i) //make sure the item is an inventoryitem
+            //        {
+            //            var sw = new SecondaryWindow();
+            //            sw.DataContext = i;
+            //            sw.Show();
+            //        }
+        }
+    }
+}
